486aa4ce2f067c98e59233467390a8ea5f141893
hemandkk <hemandkk@gmail.com>
2026-07-15 23:27:24 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
 import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
- // const user = localStorage.getItem("user_id"); // or any auth logic you use
-  const token = localStorage.getItem("access_token");
+ const user = localStorage.getItem("user_id"); // or any auth logic you use
+  //const token = localStorage.getItem("access_token");
 
-  if (!token) {
+  if (!user_id) {
     return <Navigate to="/login" replace />;
   }
 
